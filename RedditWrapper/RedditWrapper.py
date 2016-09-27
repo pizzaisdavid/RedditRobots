668e6c15e8a493b3366a800012ce2c69a144dd1f
David Germain <davidispizza@gmail.com>
2016-09-26 22:44:53 -0400

    Edit log statement

--- RedditWrapper/RedditWrapper.py
+++ RedditWrapper/RedditWrapper.py
@@ -9,7 +9,7 @@ class RedditWrapper():
         self.reddit = praw.Reddit(user_agent)
         
     def login(self, username, password):
-        self.logger.info('Logging into Reddit account: {username}'.format(username=username))
+        self.logger.info('Logging into Reddit {username} account.'.format(username=username))
         self.reddit.login(username, password, disable_warning=True)
         self.logger.info('Logged into Reddit.')
 
