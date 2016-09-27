f390b2e094ede4245b463b96a8771c69f61fef5a
David Germain <davidispizza@gmail.com>
2016-09-26 22:45:22 -0400

    Add function

--- SubmissionLimit/RedditFacade.py
+++ SubmissionLimit/RedditFacade.py
@@ -8,5 +8,23 @@ class RedditFacade(RedditWrapper):
         self.logger = logging.getLogger(__name__)
         self.logger.info('Initializing.')
         
-    def test_(self):
-        pass
+    def getSubredditSubmissionsWithin(self, subreddit_name, time_limit_in_hours):
+        self.logger.debug('Fetching submissions from {subreddit} made within the last {limit} hours.'
+                         .format(subreddit=subreddit_name, limit=time_limit_in_hours))
+        submission_limit = 100
+        submissions_within_time_frame = []
+        subreddit = self.reddit.get_subreddit(subreddit_name)
+        submissions = subreddit.get_new(limit=submission_limit)
+        for s in submissions:
+            if self.isWithinTimeLimit(s, time_limit_in_hours):
+                submissions_within_time_frame.append(s)
+            else:
+                break
+        self.logger.info('Found {count} submissions made within the past {limit} hours'
+                         .format(count=len(submissions_within_time_frame), limit=time_limit_in_hours))
+        return submissions_within_time_frame
+    
+    def isWithinTimeLimit(self, submission, time_limit_in_hours):
+        time_limit_in_seconds = time_limit_in_hours * 60 * 60
+        deadline = self.unix_timestamp - time_limit_in_seconds;
+        return deadline < submission.created_utc
