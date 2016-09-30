8d54cc2ae9be7a12ee71aba109a6eb39be2c2b01
David Germain <davidispizza@gmail.com>
2016-09-29 20:41:56 -0400

    Use variables for usernames

--- SubmissionLimit/test/test_FrequencyTable.py
+++ SubmissionLimit/test/test_FrequencyTable.py
@@ -7,15 +7,35 @@ import logging
 from .MockRedditHelper import *
 from ..src.FrequencyTable import *
 
-def test_buildUserSubmissionFrequencyTable():
+USERNAME_1 = 'Im_number_one'
+USERNAME_2 = 'someone_else'
+
+def test_buildUserSubmissionFrequencyTable_oneUser():
     submissions = [
-        makeMockSubmission('username'),
-        makeMockSubmission('username'),
-        makeMockSubmission('username')
+        makeMockSubmission(USERNAME_1),
+        makeMockSubmission(USERNAME_1),
+        makeMockSubmission(USERNAME_1)
     ]
     
     table = buildUserSubmissionFrequencyTable(submissions)
     
-    assert 'username' in table
-    assert len(table['username']) == 3
+    assert USERNAME_1 in table
+    assert len(table[USERNAME_1]) == 3
+    
+def test_buildUserSubmissionFrequencyTable_multipleUsers():
+    submissions = [
+        makeMockSubmission(USERNAME_1),
+        makeMockSubmission(USERNAME_1),
+        makeMockSubmission(USERNAME_1),
+        makeMockSubmission(USERNAME_2),
+        makeMockSubmission(USERNAME_2)
+    ]
+    
+    table = buildUserSubmissionFrequencyTable(submissions)
+    
+    assert USERNAME_1 in table
+    assert len(table[USERNAME_1]) == 3
+    
+    assert USERNAME_2 in table
+    assert len(table[USERNAME_2]) == 2
     
