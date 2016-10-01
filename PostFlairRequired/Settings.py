d71b9a7b6abdbce3522abfb51528e79ed1ef5493
David Germain <davidispizza@gmail.com>
2016-10-01 11:43:13 -0400

    New project

---
+++ PostFlairRequired/Settings.py
@@ -0,0 +1,4 @@
+user_agent = 'testing PRAW'
+subreddit_name = 'AVoid5'
+time_frame = 24
+post_limit = 1
---
+++ PostFlairRequired/src/FrequencyTable.py
@@ -0,0 +1,10 @@
+
+def buildUserSubmissionFrequencyTable(submissions):
+    table = {}
+    for s in submissions:
+        username = s.author.name
+        if username not in table:
+            table[username] = []
+        table[username].append(s)
+    
+    return table;
